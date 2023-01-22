fdcd9774ce308e78d0c55e3aaf68437d7b93aaa0
Bhavana Mali <bhavana1991mali@gmail.com>
2023-01-22 15:10:50 +0530

    Bhavana:Cucumber Project

---
+++ src/main/java/com/itview/cucumbertest/selenium_cucumber_test/App.java
@@ -0,0 +1,13 @@
+package com.itview.cucumbertest.selenium_cucumber_test;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
